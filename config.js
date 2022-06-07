c1f068197c090118ed9b49dd46bb032fab74a804
Mi206 <92164668+Mi206@users.noreply.github.com>
2022-06-07 23:51:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Long à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    title: 'Cậu có biết là tớ thích cậu lắm không😙?',
+    desc: 'Duma...tớ thề là mỗi ngày đều xem đi xem lại str của cậu...Cậu có ngại việc tiến thêm 1 bước với tớ không??',
+    btnYes: 'Ocii, đồng ý nhée <33',
+    btnNo: 'Không, tớ ngại nha🙂,
+    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại đâm vào cậu cơ chứ <3',
+    btnReply: 'Gửi cho cậu <3',
+    reply: 'Yêu cậu vch ra ýy Long ạ<33333333',
     mess: 'Haizz.....! Biết ngay mà 😆',
-    messDesc: 'Làm người yêu anh nhé ! ',
+    messDesc: 'Làm người yêu tớ nhé ! ',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/canh.cut.14224' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
