bb6a45d0cfa363efb5138c5f1454eb73c7848167
Mi206 <92164668+Mi206@users.noreply.github.com>
2022-06-07 23:54:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,39 @@
 const CONFIG = {
+
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn dĩ lạ thường
+
+    introDesc: `Trái đất vốn lạ thường
+
     Mà sao cậu cứ đi nhầm đường
-    Lạc vào tim tớ lẻ loi
+
+    Lạc vào tim tớ lẻ :<<
+
     Đằng sau chữ yêu đây là thương`,
+
     btnIntro: 'hihi',
-    title: 'Cậu có biết là tớ thích cậu lắm không😙?',
-    desc: 'Duma...tớ thề là mỗi ngày đều xem đi xem lại str của cậu...Cậu có ngại việc tiến thêm 1 bước với tớ không??',
-    btnYes: 'Ocii, đồng ý nhée <33',
-    btnNo: 'Không, tớ ngại nha🙂,
-    question:'Trên thế giới hơn 7 tỉ người mà sao tớ lại đâm vào cậu cơ chứ <3',
-    btnReply: 'Gửi cho cậu <3',
-    reply: 'Yêu cậu vch ra ýy Long ạ<33333333',
-    mess: 'Haizz.....! Biết ngay mà 😆',
-    messDesc: 'Làm người yêu tớ nhé ! ',
+
+    title: 'Cậu có biết là tớ thích cậu lắm không😙??',
+
+    desc: 'Duma, tớ thề là mỗi ngày đều xem str của cậu r lại cười...Ko biết là cậu có ngại việc tiến 1 bước với tớ không?',
+
+    btnYes: 'Ocii, đồng ý nhéee<3 ',
+
+    btnNo: 'Không, tớ ngại :) ',
+
+    question:'Trên thế giới hơn 7 tỉ người mà tớ lại đâm phải cậu rùii :>>>',
+
+    btnReply: 'Gửi cậu',
+
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+
+    messDesc: '25/6 này tớ mời cậu đi uống trà sữa nhé 👉👈.',
+
     btnAccept: 'Okiiiii lun <3',
+
     messLink: 'https://www.facebook.com/canh.cut.14224' //link mess của các bạn. VD: https://m.me/nam.nodemy
+
 }
+    
+    
